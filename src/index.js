6d3597e7d07019b16d6ff5a5c1d5505224ef54e3
pappagallomatto <109523191+pappagallomatto@users.noreply.github.com>
2022-07-19 16:46:36 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
+import Phaser, { Time } from 'phaser';
 
-import Phaser from "phaser";
-
-const config = {
+var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  debug: true,
+  scene: {
+      preload,
+      create,
+      update
+  },
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
     }
-  },
-  scene: {
-    preload: preload,
-    create: create
   }
 };
 
-new Phaser.Game(config);
+var game = new Phaser.Game(config);
+let bird = null;
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+function preload ()
+{
+  this.load.image('sky','/assets/sky.png');
+  this.load.image('bird','/assets/bird.png');
+  this.load.image("ground", "/assests/platform.png");
+  let space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  space.on('down',fly);
+  
+}
+var platforms;
+function create ()
+{
+  this.add.image(0,0,'sky').setOrigin(0);
+  bird = this.physics.add.sprite(0,300, 'bird').setOrigin(0);
+  bird.body.gravity.y= 200;
+  
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+function update ()
+{
 }
+
+function fly() {
+ bird.body.velocity.y = -200;
+ bird.body.velocity.x = 70
+}
+
